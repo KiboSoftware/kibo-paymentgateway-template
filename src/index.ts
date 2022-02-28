b1c597abfdc34ee7cef52db15d1633dfeae59916
Kevin Watts <kevin.watts@kibocommerce.com>
2022-02-28 11:51:01 -0600

    fix: remove console log and add no-console rule to linter

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { CustomAdapterFactory } from './CustomAdapterFactory'
 import config from './Config'
 
 const settings = config.get('settings')
-console.log(settings)
 const factory = new CustomAdapterFactory(settings)
 
 host(factory)
